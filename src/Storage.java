e9f7a82fcdba4fd50ae8ea50026d46a0209ca193
leowweixiang <a0097299@nus.edu.sg>
2014-09-23 13:18:36 +0800

    Created basic template Imported JSON Set up git

---
+++ src/Storage.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import org.json.*;
+
+public class Storage {
+	
+	
+	public void add(JSONObject json) {
+
+	}
+	
+	public void delete(JSONObject json) {
+		
+	}
+	
+	public void edit(JSONObject json) {
+		
+	}
+	
+	public ArrayList<JSONObject> search(String keyword, String tag, ArrayList<String> dates) {
+
+	}
+}
