d5eacef7f9733c420e582aebcaa35b0b0314654b
Daryl Tan <daryltanzw@gmail.com>
2014-09-25 16:08:18 +0800

    Storage modue as of 25/9/2014

--- src/Storage.java
+++ src/Storage.java
@@ -1,45 +1,158 @@
 import java.util.*;
 import java.io.*;
-import org.json.*;
 
-/*
- * Tasks contain
- * -ID
- * -Description
- * -Tags
- * -Date(s) and time(s)
- * -Recurring (Weekly, Yearly, Monthly)
- * -Overdue (boolean)
- */
+import org.json.JSONException;
+
+import com.google.gson.Gson;
 
 public class Storage {
 	private ArrayList<Task> al_task;
 	private ArrayList<Task> al_task_floating;
 	private ArrayList<Task> al_task_overdue;
+	private ArrayList<Task> al_task_completed;
 	private int counter;
-	
-	public Storage(){
-		
-		//read files into arrayList.
+
+	private BufferedReader bufferedReader;
+	private PrintWriter printWriter;
+	private static final String FLOATING_TASK_FILENAME = "C:\\Users\\Daryl\\git\\2103Proj\\src\\FloatingTask.txt";
+	private static final String COMPLETED_TASK_FILENAME = "C:\\Users\\Daryl\\git\\2103Proj\\src\\CompletedTask.txt";
+	private static final String OVERDUE_TASK_FILENAME = "C:\\Users\\Daryl\\git\\2103Proj\\src\\OverdueTask.txt";
+	private static final String TASK_FILENAME = "C:\\Users\\Daryl\\git\\2103Proj\\src\\Task.txt";
+	private static final String COUNT_TASK_FILENAME = "C:\\Users\\Daryl\\git\\2103Proj\\src\\TaskCount.txt";
+
+	// Index for if the task does not exist in the ArrayList.
+	private static final int DOES_NOT_EXIST = -1;
+
+	public Storage() throws IOException {
+		al_task = new ArrayList<Task>();
+		al_task_floating = new ArrayList<Task>();
+		al_task_overdue = new ArrayList<Task>();
+		al_task_completed = new ArrayList<Task>();
+		initFiles();
 	}
 
-	public void add(JSONObject json) {
+	// if exists, replace task. Else add task.
+	public void insert(Task task, ArrayList<Task> file) throws JSONException,
+			IOException {
+		readFile(file);
+		int taskIndex = getIndex(file, task);
+
+		// add
+		if (taskIndex == DOES_NOT_EXIST) {
+			counter = Integer.parseInt(getTaskCounter()) + 1;
+			task.setTaskId(counter);
+			file.add(task);
+			writeTaskCounter(counter);
+		}
+		// insert
+		else {
+			file.set(taskIndex, task);
+		}
+		writeFile(file);
+	}
+
+	public void delete(Task task, ArrayList<Task> file) throws IOException {
+		readFile(file);
+		int taskIndex = getIndex(file, task);
+		if (taskIndex != DOES_NOT_EXIST) {
+			file.remove(taskIndex);
+		}
+		writeFile(file);
+	}
+
+	public ArrayList<Task> getTasksFile() {
+		return this.al_task;
+	}
+
+	public ArrayList<Task> getFloatingTasksFile() {
+		return this.al_task_floating;
+	}
+
+	public ArrayList<Task> getCompletedTasksFile() {
+		return this.al_task_completed;
+	}
+
+	public ArrayList<Task> getOverdueTasksFile() {
+		return this.al_task_overdue;
+	}
+	
+	//Methods Not Accessible to Storage instance.****************************
+	private void initFiles() throws IOException {
+		readFile(al_task);
+		readFile(al_task_floating);
+		readFile(al_task_completed);
+		readFile(al_task_overdue);
+	}
+
+	private int getIndex(ArrayList<Task> file, Task task) {
+		for (int i = 0; i < file.size(); i++) {
+			if (task.getTaskId().equals(file.get(i).getTaskId())) {
+				return i;
+			}
+		}
+		return DOES_NOT_EXIST;
+	}
+
+	// Interfaces with the textFiles(databases)*************************
+	private String getTaskCounter() throws IOException {
+		bufferedReader = new BufferedReader(new FileReader(COUNT_TASK_FILENAME));
+		
+		String result = bufferedReader.readLine();
+		if(result == null){
+			return "0";
+		}
+		else{
+			return result;
+		}
+	}
+
+	private void writeTaskCounter(int count) throws FileNotFoundException {
+		printWriter = new PrintWriter(new FileOutputStream(COUNT_TASK_FILENAME));
+		printWriter.println("" + count);
+		printWriter.close();
+	}
+
+	private void readFile(ArrayList<Task> file) throws IOException {
+		String fileName = determineFileName(file);
+		bufferedReader = new BufferedReader(new FileReader(fileName));
+		Gson gson = new Gson();
+		String line = null;
+		file.removeAll(file);
+		while ((line = bufferedReader.readLine()) != null) {
+			Task task = gson.fromJson(line, Task.class);
+			file.add(task);
+		}
 
 	}
-	
-	public void delete(JSONObject json) {
-		
+
+	private void writeFile(ArrayList<Task> fileToWrite)
+			throws FileNotFoundException {
+
+		String filename = determineFileName(fileToWrite);
+		// Start writing
+		printWriter = new PrintWriter(new FileOutputStream(filename));
+		Gson gson = new Gson();
+		for (Task task : fileToWrite) {
+			String write = gson.toJson(task);
+			printWriter.println(write);
+		}
+		printWriter.close();
 	}
-	
-	public void edit(JSONObject json) {
-		
+
+	// Interfaces with the textFiles(databases)*************************
+	private String determineFileName(ArrayList<Task> fileToWrite) {
+		String filename = "";
+		if (fileToWrite.equals(al_task)) {
+			filename = TASK_FILENAME;
+		} else if (fileToWrite.equals(al_task_floating)) {
+			filename = FLOATING_TASK_FILENAME;
+		} else if (fileToWrite.equals(al_task_completed)) {
+			filename = COMPLETED_TASK_FILENAME;
+		} else if (fileToWrite.equals(al_task_overdue)) {
+			filename = OVERDUE_TASK_FILENAME;
+		} else {
+			throw new Error("Invalid file to write to");
+		}
+		return filename;
 	}
-	
-	/*
-	 * Searches for keywords, tags within date range.
-	 */
-	//public ArrayList<JSONObject> search(String keyword, String tag, ArrayList<String> dates) {
-		//if no keyword, no tag and no date, throw error
-		//if no date, search for all dates
-//	}
 }
--- src/Task.java
+++ src/Task.java
@@ -1,34 +1,123 @@
+
 import java.util.ArrayList;
 import java.util.Date;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
 public class Task {
-	private JSONObject obj;
-	
-	private static final String TASK_NAME = "taskname";
-	private static final String TASK_ID = "ID";
-	private static final String TASK_DATES_TIMES = "taskDteTimes";
-	private static final String TASK_REMINDER_DATES_TIMES = "reminderDteTimes";
-	private static final String TASK_FLOATING = "isFloating";
-	private static final String TASK_RECUR = "recurTasks"; 
-	private static final String TASK_COMPLETED = "isCompleted";
-	private static final String TASK_TAGS = "tags";
-	
-	private static final String emptyVal = "";
-	
-	public Task() throws JSONException{
-		obj = new JSONObject();
-		obj.put(TASK_ID, emptyVal);
-		obj.put(TASK_NAME, emptyVal);
-		obj.put(TASK_DATES_TIMES, new ArrayList<Date>());
-		obj.put(TASK_REMINDER_DATES_TIMES, new ArrayList<Date>());
-		obj.put(TASK_FLOATING, false);
-		obj.put(TASK_RECUR, emptyVal);
-		obj.put(TASK_COMPLETED, false);
-		obj.put(TASK_TAGS, new ArrayList<String>());
+	private String taskName;
+	private String taskId;
+	private ArrayList<Date> taskDatesTimes;
+	private ArrayList<Date> taskReminderDatesTimes;
+	private boolean taskFloating;
+	private String taskRecur;
+	private boolean taskCompleted;
+	private ArrayList<String> taskTag;
+		
+	public Task() {
+		taskId = "";
+		taskName = "";
+		taskDatesTimes = new ArrayList<Date>();
+		taskReminderDatesTimes = new ArrayList<Date>();
+		taskFloating = false;
+		taskRecur = "";
+		taskCompleted = false;
+		taskTag = new ArrayList<String>();
+	}
+
+	// Task ID************************************
+	public void setTaskId(int id) {
+		this.taskId = "" + id;
+	}
+
+	public String getTaskId() {
+		return this.taskId;
+	}
+
+	// Task ID************************************
+
+	// Task Name ************************************
+	public void setTaskName(String taskname) {
+		this.taskName = taskname;
+	}
+
+	public String getTaskName() {
+		return this.taskName;
+	}
+
+	// Task Name ************************************
+
+	// Task Dates and Times ********************************
+	public void setTaskDatesTimes(ArrayList<Date> al){
+		this.taskDatesTimes = al;
+	}
+	public void removeTaskDatesTimes(Date date){
+		this.taskDatesTimes.remove(date);
+	}
+	public void addTaskDatesTimes(Date date) {
+		this.taskDatesTimes.add(date);
+	}
+
+	public ArrayList<Date> getTaskDatesTimes() {
+		return this.taskDatesTimes;
+	}
+	// Task Dates and Times ********************************
+
+	// Task Reminder Dates Times***********************************
+	public void setTaskReminderDatesTimes(ArrayList<Date> al){
+		this.taskReminderDatesTimes = al;
+	}
+	public void removeTaskReminderDatesTimes(Date date){
+		this.taskReminderDatesTimes.remove(date);
+	}
+	public void addTaskReminderDatesTimes(Date date) {
+		this.taskReminderDatesTimes.add(date);
+	}
+
+	public ArrayList<Date> getTaskReminderDatesTimes() {
+		return this.taskReminderDatesTimes;
+	}
+
+	// Task Reminder Dates Times***********************************
+
+	// Task Floating*******************************
+	public void setTaskFloating(boolean b) {
+		this.taskFloating = b;
+	}
+
+	public boolean isTaskFloating() {
+		return this.taskFloating;
+	}
+
+	// Task Floating*******************************
+
+	// Task Recur***************************************
+	public void setTaskRecur(String str) {
+		this.taskRecur = str;
+	}
+
+	public String getTaskRecur() {
+		return this.taskRecur;
+	}
+
+	// Task Recur***************************************
+
+	// Task Completed*********************************************
+	public void setTaskCompleted(boolean b) {
+		this.taskCompleted = b;
+	}
+
+	public boolean getTaskCompleted() {
+		return this.taskCompleted;
+	}
+
+	// Task Completed*********************************************
+
+	// Task Tags**************************************
+	public void addTaskTags(String str) {
+		this.taskTag.add(str);
+	}
+
+	public ArrayList<String> getTaskTags() {
+		return this.taskTag;
 	}
 	
 }
