23902dd4b996d0a3c14e5a3a217eeef618a3a75f
Wu Di <justwudi@gmail.com>
2014-10-06 02:07:00 +0800

    final not allowed here

--- src/Parser.java
+++ src/Parser.java
@@ -2,11 +2,11 @@ public class Parser {
 
 	private String command;
 
-	public final enum COMMAND_TYPE {
+	public enum COMMAND_TYPE {
 		ADD, EDIT, DELETE, LIST, SEARCH, COMPLETE, TAG
 	}
 
-	public final enum RECURRING_TYPE {
+	public enum RECURRING_TYPE {
 		INTERVAL, DAY_OF_MONTH, WEEK_OF_MONTH, ANNUAL
 	}
 
