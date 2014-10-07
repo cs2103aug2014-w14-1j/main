1123b028e1ddc9e99257cee2d75a8993b0d3272a
Wu Di <justwudi@gmail.com>
2014-10-07 15:54:17 +0800

    Add getFirstWord method

--- src/Parser.java
+++ src/Parser.java
@@ -10,11 +10,16 @@ public class Parser {
 		INTERVAL, DAY_OF_MONTH, WEEK_OF_MONTH, ANNUAL
 	}
 
-	public void parseCommand() {
-
+	public void parseCommand(String userCommand) {
+		command = userCommand;
 	}
 
 	public boolean isValidCommand() {
+		return false;
+	}
+	
+	private String getFirstWord(String input) {
+		return input.split("\\s+")[0];
 
 	}
 }
