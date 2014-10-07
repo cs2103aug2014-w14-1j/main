c7508648fa3982135075b9cd3591797182bcc05f
Wu Di <justwudi@gmail.com>
2014-10-07 16:45:53 +0800

    Rewrite command class with setter and getter for command type

--- src/Command.java
+++ src/Command.java
@@ -12,75 +12,11 @@ public class Command {
 	private String taskEndDate;
 	private LinkedList<String> taskTags;
 
+	public void setCommandType(COMMAND_TYPE parsedCommandType) {
+		commandType = parsedCommandType;
+	}
+
 	public COMMAND_TYPE getCommandType() {
-
-	}
-
-	public String getTaskName() {
-
-	}
-
-	public String getTaskId() {
-
-	}
-
-	public Calendar getTaskStartDate() {
-
-	}
-
-	public Calendar getTaskEndDate() {
-
-	}
-
-	public LinkedList<String> getTaskTags() {
-
-	}
-
-	public RECURRING_TYPE getRecurringType() {
-
-	}
-
-	public int getRecurringTaskInterval() {
-
-	}
-
-	public int getRecurringTaskDayOfMonth() {
-
-	}
-
-	public int getRecurringTaskWeekOfMonth() {
-
-	}
-
-	public int getRecurringTaskWeekOfMonthDay() {
-
-	}
-
-	public int getRecurringTaskMonthInterval() {
-
-	}
-
-	public int getRecurringTaskAnnualMonth() {
-
-	}
-
-	public int getRecurringTaskAnnualDay() {
-
-	}
-
-	public String getSearchTerm() {
-
-	}
-
-	public Calendar getSearchRangeStartDate() {
-
-	}
-
-	public Calendar getSearchRangeEndDate() {
-
-	}
-
-	public ArrayList<String> getSearchRangeTags() {
-
+		return commandType;
 	}
 }
